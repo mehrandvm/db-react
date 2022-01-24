d7cc5ae8d88c4a15d0594c5112884751870f5f19
mehrandvm <amir.daneshvarmoein@gmail.com>
2022-01-24 17:26:53 +0330

    removed bloatware styles

---
+++ src/components/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/app.module.scss';
+
+const App = () => {
+  return <div className={styles.app}>Hello, World!</div>;
+};
+
+export default App;
