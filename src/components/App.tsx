d3dd8c6223b6f06bee904540230db428c0bde152
mehrandvm <amir.daneshvarmoein@gmail.com>
2022-01-24 20:31:59 +0330

    added theme to App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { LanguageProvider } from '../context/language';
 import Home from './home/Home';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { StylesProvider } from '@mui/styles';
+import { theme } from '../styles/theme';
 
 const App = () => {
   return (
     <LanguageProvider userLanguage="fa">
-      <Home />
+      <ThemeProvider theme={theme}>
+        <StylesProvider injectFirst>
+          <CssBaseline enableColorScheme />
+          <Home />
+        </StylesProvider>
+      </ThemeProvider>
     </LanguageProvider>
   );
 };
