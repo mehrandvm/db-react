db60aa09e48757980998969e6f8973cd6d7fbd9a
mehrandvm <amir.daneshvarmoein@gmail.com>
2022-01-27 17:45:30 +0330

    created panel router

---
+++ src/router/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import DashboardLayout from '../layout/dashboard';
+import LoginLayout from '../layout/login';
+
+const PanelRouter = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginLayout />} />
+      <Route path="/" element={<DashboardLayout />} />
+    </Routes>
+  );
+};
+
+export default PanelRouter;
